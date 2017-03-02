7d190f1cc45455301e231c2c1a5a2b3f4d80ca60
pratheeksh <pks329@nyu.edu>
2017-03-02 16:00:59 -0500

    modified script to save model

--- Assign1/script/Supervised-augmentations.py
+++ Assign1/script/Supervised-augmentations.py
@@ -1,35 +1,17 @@
-
-# coding: utf-8
-
-# In[1]:
-
-get_ipython().magic('matplotlib inline')
-
 from __future__ import print_function
-import pickle 
+
+import pickle
+import random
+
+import matplotlib.pyplot as plt
 import numpy as np
-import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.autograd import Variable
-
-
-# In[2]:
-
-import PIL
-from PIL import Image
-import matplotlib.pyplot as plt
-import random
-import math
-
-import scipy
 from scipy import ndimage
-
-import time
-
+from torch.autograd import Variable
+from torchvision import transforms
 
 # # Split Data
 
@@ -37,48 +19,60 @@ import time
 
 # In[4]:
 
-trainset_imoprt = pickle.load(open("../data/kaggle/train_labeled.p", "rb"))
+trainset_labeled_import = pickle.load(open("../data/kaggle/train_labeled.p", "rb"))
+trainset_unlabeled_import = pickle.load(open("../data/kaggle/train_unlabeled.p", "rb"))
 validset_import = pickle.load(open("../data/kaggle/validation.p", "rb"))
 
+train_labeled_loader = torch.utils.data.DataLoader(trainset_labeled_import, batch_size=32, shuffle=True)
+train_unlabeled_loader = torch.utils.data.DataLoader(trainset_unlabeled_import, batch_size=256,
+                                                     shuffle=True)
 
-# In[5]:
-
-train_loader = torch.utils.data.DataLoader(trainset_imoprt, batch_size=64, shuffle=True)
-valid_loader = torch.utils.data.DataLoader(validset_import, batch_size=64, shuffle=True)
+train_unlabeled_loader.dataset.train_labels = [-1 for i in range(len(train_unlabeled_loader.dataset.train_data))]
+valid_loader = torch.utils.data.DataLoader(validset_import, batch_size=32, shuffle=True)
 
 
 # In[6]:
 
-class Net(nn.Module):
+class NetBN(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        super(NetBN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=5, stride=1, padding=1,
+                               bias=False)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=1,
+                               bias=False)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=5, stride=1, padding=1,
+                               bias=False)
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=0, padding=0, ceil_mode=True)  # change
+
+        self.conv1_bn = nn.BatchNorm2d(64)
+        self.conv2_bn = nn.BatchNorm2d(128)
+        self.conv3_bn = nn.BatchNorm2d(256)
+        self.conv1_drop = nn.Dropout2d(0.2)
         self.conv2_drop = nn.Dropout2d(0.3)
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.conv3_drop = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(32 * 32, 500)
+        self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
+        x = self.conv1_drop(F.relu(self.maxpool(self.conv1_bn(self.conv1(x)))))
+
+        x = self.conv2_drop(F.relu(self.maxpool(self.conv2_bn(self.conv2(x)))))
+
+        x = self.conv3_drop(F.relu(self.maxpool(self.conv3_bn(self.conv3(x)))))
+        x = x.view(-1, 32 * 32)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = F.relu(self.fc2(x))
         return F.log_softmax(x)
 
 
-
-# In[ ]:
-
-import model
-model = pickle.load(open("model.p", "rb"))
-
+model = NetBN()
 
 # In[7]:
 
 # optim.SGD(params, lr=<object>, momentum=0, dampening=0, weight_decay=0)
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.8)
+
 
 # optim.Adam(params, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
 # optimizer = optim.Adam(model.parameters(), lr=args.lr)
@@ -87,10 +81,11 @@ optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.8)
 # In[152]:
 
 # Single channel only
-def tensor_imshow(tensor):    
+def tensor_imshow(tensor):
     npimg = tensor.numpy()[0]
     np_imshow(npimg)
-    
+
+
 def np_imshow(npimg):
     plt.figure(1)
     plt.imshow(npimg, cmap='gray')
@@ -100,7 +95,6 @@ def np_imshow(npimg):
 # In[194]:
 
 def clipped_zoom(img, zoom_factor, **kwargs):
-
     h, w = img.shape[:2]
 
     # width and height of the zoomed image
@@ -120,19 +114,19 @@ def clipped_zoom(img, zoom_factor, **kwargs):
         # zero-padding
         out = np.zeros_like(img)
         out += np.amin(img)
-        out[top:top+zh, left:left+zw] = ndimage.zoom(img, zoom_tuple, **kwargs)
+        out[top:top + zh, left:left + zw] = ndimage.zoom(img, zoom_tuple, **kwargs)
 
     # zooming in
     elif zoom_factor > 1:
         # bounding box of the clip region within the input array
         top = (zh - h) // 2
         left = (zw - w) // 2
-        out = ndimage.zoom(img[top:top+zh, left:left+zw], zoom_tuple, **kwargs)
+        out = ndimage.zoom(img[top:top + zh, left:left + zw], zoom_tuple, **kwargs)
         # `out` might still be slightly larger than `img` due to rounding, so
         # trim off any extra pixels at the edges
         trim_top = ((out.shape[0] - h) // 2)
         trim_left = ((out.shape[1] - w) // 2)
-        out = out[trim_top:trim_top+h, trim_left:trim_left+w]
+        out = out[trim_top:trim_top + h, trim_left:trim_left + w]
 
     # if zoom_factor == 1, just return the input array
     else:
@@ -152,27 +146,24 @@ def to_npimg(tensor):
 
 
 def random_translate(npimg, min_translate=-0.2, max_translate=0.2):
-    
     h, w = npimg.shape
     min_pixels = ((h + w) / 2) * min_translate
     max_pixels = ((h + w) / 2) * max_translate
     shift = int(random.uniform(min_pixels, max_pixels))
-    
+
     return ndimage.interpolation.shift(npimg, shift, mode='nearest')
 
 
 def random_rotate(npimg, min_rotate=-30, max_rotate=30):
-    
     theta = random.randint(int(min_rotate), int(max_rotate + 1))
     return ndimage.interpolation.rotate(npimg, theta, reshape=False, mode='nearest')
 
 
 def random_scale(npimg, min_scale=0.5, max_scale=1.5):
-    
     scale_factor = random.uniform(min_scale, max_scale)
     sc_img = clipped_zoom(npimg, scale_factor, mode='nearest')
     return sc_img
-    
+
 
 # Converts a HxW ndarray into a 1xHxW tensor
 def to_tensor(npimg):
@@ -184,64 +175,99 @@ def to_tensor(npimg):
 # In[196]:
 
 # CPU only training
-def train(epoch):
+def train_without_jitter(epoch):
     model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
+    for batch_idx, (data, target) in enumerate(train_labeled_loader):
+        data, target = Variable(data), Variable(target)
+
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 20 == 0:
+            print('Train - Jitterfree Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_labeled_loader.dataset),
+                       100. * batch_idx / len(train_labeled_loader), loss.data[0]))
+
+
+def train_with_jitter(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_labeled_loader):
 
         for i in range(len(data)):
-            
             data[i] = transforms.Compose([
                 transforms.Lambda(lambda x: to_npimg(x)),
                 transforms.Lambda(lambda x: random_translate(x)),
                 transforms.Lambda(lambda x: random_rotate(x)),
                 transforms.Lambda(lambda x: random_scale(x)),
                 transforms.Lambda(lambda x: to_tensor(x)),
-                ])(data[i])
-        
+            ])(data[i])
+
         data, target = Variable(data), Variable(target)
-        
+
         optimizer.zero_grad()
         output = model(data)
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % 10 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data[0]))
+        if batch_idx % 20 == 0:
+            print('Train - Jitter Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_labeled_loader.dataset),
+                       100. * batch_idx / len(train_labeled_loader), loss.data[0]))
 
 
-# In[197]:
-
-train(1)
-
-
-# In[198]:
-
-def test(epoch, valid_loader):
+def validate(epoch, valid_loader):
     model.eval()
     test_loss = 0
     correct = 0
     for data, target in valid_loader:
-
         data, target = Variable(data, volatile=True), Variable(target)
         output = model(data)
-        test_loss += F.nll_loss(output, target).data[0]
-        pred = output.data.max(1)[1] # get the index of the max log-probability
+        test_loss += F.nll_loss(output, target)
+
+        pred = output.data.max(1)[1]  # get the index of the max log-probability
         correct += pred.eq(target.data).cpu().sum()
 
-    test_loss /= len(valid_loader) # loss function already averages over batch size
+    test_loss /= len(valid_loader)  # loss function already averages over batch size
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(valid_loader.dataset),
+        test_loss.data.numpy()[0], correct, len(valid_loader.dataset),
         100. * correct / len(valid_loader.dataset)))
+
+
+def train_unlabeled(epoch):
+    model.train()
+    for batch_idx, (data, _) in enumerate(train_unlabeled_loader):
+        data = Variable(data)
+        optimizer.zero_grad()
+        output = model(data)
+        target = Variable(torch.LongTensor(output.data.max(1)[1].numpy().reshape(-1)))
+        #         loss = my_criterion.forward(output, target, Variable(torch.LongTensor(epoch)),
+        # Variable(torch.LongTensor(2)))
+        #         my_criterion.backward(loss)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 20 == 0:
+            print('Train Unlabeled Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_unlabeled_loader.dataset),
+                       100. * batch_idx / len(train_unlabeled_loader), loss.data[0]))
+
+
+# In[198]:
 
 
 # In[199]:
 
-for epoch in range(1, 10):
-    train(epoch)
-    test(epoch, valid_loader)
+for epoch in range(1, 60):
+    train_without_jitter(epoch)
+    train_with_jitter(epoch)
+    pickle.dump(model, open("model_with_jitter.p", "wb"))
+    if epoch > 40:
+        train_unlabeled(epoch)
+    pickle.dump(model, open("model_with_jitter_unlabeld.p", "wb"))
 
+    validate(epoch, valid_loader)
 
 # # Create Sample Submission
 
@@ -249,25 +275,7 @@ for epoch in range(1, 10):
 
 testset = pickle.load(open("../data/kaggle/test.p", "rb"))
 
-
-# In[ ]:
-
-
-
-
-# In[11]:
-
-test_loader = torch.utils.data.DataLoader(testset,batch_size=64, shuffle=False)
-
-
-# ## Test Accuuracy
-
-# In[12]:
-
-test(1, test_loader)
-
-
-# In[13]:
+test_loader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
 
 label_predict = np.array([])
 model.eval()
@@ -277,24 +285,21 @@ for data, target in test_loader:
     temp = output.data.max(1)[1].numpy().reshape(-1)
     label_predict = np.concatenate((label_predict, temp))
 
-
 # In[14]:
 
 label_predict
 
-
 # In[17]:
 
 import pandas as pd
+
 predict_label = pd.DataFrame(label_predict, columns=['label'], dtype=int)
 predict_label.reset_index(inplace=True)
 predict_label.rename(columns={'index': 'ID'}, inplace=True)
 
-
 # In[18]:
 
 predict_label.head()
-
 
 # In[19]:
 
@@ -302,6 +307,3 @@ predict_label.to_csv('../data/kaggle/sample_submission.csv', index=False)
 
 
 # In[ ]:
-
-
-
