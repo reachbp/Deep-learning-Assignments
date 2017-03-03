f2979b1334601be9d5f949e0a434c5104f702ed9
pratheeksh <pks329@nyu.edu>
2017-03-03 14:34:11 -0500

    added gaussian noise

--- Assign1/script/Supervised-augmentations.py
+++ Assign1/script/Supervised-augmentations.py
@@ -288,22 +288,22 @@ def train_unlabeled(epoch):
 
 # In[199]:
 #
-# for epoch in range(1, 80):
-#     train_without_jitter(epoch)
-#     train_with_jitter(epoch)
-#     if epoch == 40:
-#         pickle.dump(model, open("model_with_jitter.p", "wb"))
-#     if epoch > 40:
-#         train_unlabeled(epoch)
-#     if epoch == 79:
-#         pickle.dump(model, open("model_with_jitter_unlabeld.p", "wb"))
-#
-#     validate(epoch, valid_loader)
+for epoch in range(1, 100):
+    train_without_jitter(epoch)
+    train_with_jitter(epoch)
+    if epoch == 40:
+        pickle.dump(model, open("model_labeled_mar3.p", "wb"))
+    if epoch > 50:
+        train_unlabeled(epoch)
+    if epoch == 99:
+        pickle.dump(model, open("model_unlabeld_mar3.p", "wb"))
+
+    validate(epoch, valid_loader)
 
 # # Create Sample Submission
 
 # In[10]:
-model = pickle.load(open("good_model.p", "rb"))
+# model = pickle.load(open("good_model.p", "rb"))
 
 testset = pickle.load(open("../data/kaggle/test.p", "rb"))
 
