c8eeb2bd7e7cc2b10c8c2adbb6f438e82a8e9933
Bharathi Priyaa <bt978@nyu.edu>
2017-03-30 16:10:45 -0400

    Removing files

--- Assign2/code/script.sh
+++
@@ -1,8 +0,0 @@
-module purge
-module load python/intel/2.7.12
-# module load python3/intel/3.5.3
-module load scikit-learn/intel/0.18.1
-module load torch/intel/20170104
-module load pytorch/intel/20170125
-module load torchvision/0.1.7
-
