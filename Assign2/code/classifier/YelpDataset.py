1a27b1c8df5cde158891a6639feea0cfd805e130
Bharathi Priyaa <bt978@nyu.edu>
2017-03-30 23:02:47 -0400

    Pruning words with freq < 5

--- Assign2/code/classifier/YelpDataset.py
+++ Assign2/code/classifier/YelpDataset.py
@@ -10,11 +10,14 @@ class Dictionary(object):
     def __init__(self):
         self.word2idx = {}
         self.idx2word = []
+        self.word2count = {}
 	
     def add_word(self, word):
         if word not in self.word2idx:
             self.idx2word.append(word)
             self.word2idx[word] = len(self.idx2word) - 1
+            self.word2count[word] = 0
+        self.word2count[word] = self.word2count[word] + 1
         return self.word2idx[word]
 
     def __len__(self):
@@ -35,6 +38,19 @@ class YelpDataset(object):
             review = json.loads(line)
             self.processReviewText(review['text'])
 
+        print("Size in the vocabulary ",len(self.dictionary.word2idx.keys()) )
+        # Prune words with frequency of 50 or less
+        for word in self.dictionary.word2count.keys():
+            if  self.dictionary.word2count[word] < 5:
+                index_to_remove = self.dictionary.word2idx[word]
+                last_index = self.dictionary.__len__() - 1
+                last_word = self.dictionary.idx2word[last_index]
+                self.dictionary.idx2word[index_to_remove] = last_word
+                self.dictionary.word2idx[last_word] = index_to_remove
+                self.dictionary.word2idx.__delitem__(word)
+                self.dictionary.idx2word.pop()
+        print("Reviews size in the vocabulary ",len(self.dictionary.word2idx.keys()), self.dictionary.__len__() )
+        print(self.dictionary.word2count)
         for line in open(filename, 'r'):
             review = json.loads(line)
             self.data.append(self.tokenizeReviewText(review['text']))
@@ -42,7 +58,7 @@ class YelpDataset(object):
 
         print("============== All reviews read into list ==============")
 
-        print("Number of reviews loaded ", len(self.data), len(self.target))
+
         pickle.dump(self.data, open('data.pkl', 'wb'))
         pickle.dump(self.target, open('target.pkl', 'wb'))
         print("Words in the vocabulary ",self.dictionary.word2idx )
@@ -54,14 +70,16 @@ class YelpDataset(object):
         for word in text.split():
             self.dictionary.add_word(word)
 
+
     def tokenizeReviewText(self, text):
         text = re.sub('[^A-Za-z0-9\s]+', ' ', text).lower()
         numWords = len(text.split())
         ids = np.zeros(numWords+1)
         token = 0
         for word in text.split():
-            ids[token] = self.dictionary.word2idx[word]
-            token += 1
+            if word in self.dictionary.word2idx.keys():
+                ids[token] = self.dictionary.word2idx[word]
+                token += 1
         return ids[0:50]
 
 class ReviewPair(object):
@@ -71,7 +89,7 @@ class ReviewPair(object):
 
 
 def main():
-    filename = "../data/sentiment/yelp_training_set_review.json"
+    filename = "../data/sentiment/dummy.json"
     dataset = YelpDataset(filename)
     dataset.loadDataset(filename)
 
