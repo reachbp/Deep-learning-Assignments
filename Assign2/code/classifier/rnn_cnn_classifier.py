89f45bcd0dbfe6262666c5eea4d7779634388e5c
pratheeksh <pks329@nyu.edu>
2017-04-07 19:04:04 +0000

    rnn-cnn-classifier

---
+++ Assign2/code/classifier/rnn_cnn_classifier.py
@@ -0,0 +1,80 @@
+__author__ = 'bharathipriyaa'
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+'''
+class RNNModel(nn.Module):
+    def __init__(self, ntoken, input_size, hidden_size, output_size, batchsize):
+        super(RNNModel, self).__init__()
+        self.encoder = nn.Embedding(ntoken, input_size)
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax()
+
+    def forward(self, input, hidden):
+        emb = self.encoder(input)
+        print("Embedding size", emb.size())
+        print("Size of input and hidden", input.size(), hidden.size())
+        combined = torch.cat((input, hidden), 1)
+        print("Checkpoint 1 ", combined.size(), self.input_size + self.hidden_size)
+        hidden = self.i2h(combined)
+        print("Checkpoint 2 ", hidden.size())
+        output = self.i2o(combined)
+        print("Checkpoint 3 ", output.size())
+        output = self.softmax(output)
+        return output, hidden
+
+    def init_hidden(self, batchsize):
+        return Variable(torch.zeros(batchsize, self.hidden_size))
+'''
+class RNNModelCNN(nn.Module):
+    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, batchsize, bptt):
+        super(RNNModelCNN, self).__init__()
+        self.encoder = nn.Embedding(ntoken, ninp)
+        self.conv1 = nn.Conv1d(bptt,bptt, 3, stride = 1)
+        self.maxpool =F.max_pool1d
+	self.rnn = getattr(nn, rnn_type)(149, nhid, nlayers, bias=False)
+        self.decoder1 = nn.Linear(nhid*bptt, 500)
+        self.decoder2 = nn.Linear(500, 2)
+        self.init_weights()
+
+        self.rnn_type = rnn_type
+        self.nhid = nhid
+        self.nlayers = nlayers
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder1.bias.data.fill_(0)
+        self.decoder1.weight.data.uniform_(-initrange, initrange)
+        self.decoder2.bias.data.fill_(0)
+        self.decoder2.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, input, hidden):
+        emb = self.encoder(input)
+        #print("Embedding size ", emb.size(), input.size())
+        x = self.conv1(emb)
+        #print(x.size())
+        x = self.maxpool(x, 2)
+        #print(x.size())
+        output, hidden = self.rnn(x, hidden)
+        #print("Check point 1", output.size(), hidden.size())
+        output = output.view(-1,output.size(1)* output.size(2))
+        #print("Check point 2", output.size())
+        decoded = F.relu(self.decoder1(output))
+        decoded = F.relu(self.decoder2(decoded))
+        #print("Check point 3", decoded.size())
+        return F.log_softmax(decoded), hidden
+
+    def init_hidden(self, bsz):
+        weight = next(self.parameters()).data
+        if self.rnn_type == 'LSTM':
+            return (Variable(weight.new(self.nlayers, bsz, self.nhid).zero_()),
+                    Variable(weight.new(self.nlayers, bsz, self.nhid).zero_()))
+        else:
+            return Variable(weight.new(self.nlayers, bsz, self.nhid).zero_())
